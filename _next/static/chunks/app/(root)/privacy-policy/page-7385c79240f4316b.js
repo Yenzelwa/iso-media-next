3856293fd28bb931bf7896774742e0795e2caee8
Nokukhanya Dumakude <NokukhanyaD@hollywoodbets.net>
2024-08-26 07:49:18 +0200

    Updates

--- _next/static/chunks/app/(root)/privacy-policy/page-7385c79240f4316b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[111,907,42,595,929,641,549,102],{7155:function(){}},function(n){n.O(0,[667,139,744],function(){return n(n.s=7155)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/(root)/terms-conditions/page-8e1d677efda6c0f1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[102,907,641,595,549,929,42,111],{7155:function(){}},function(n){n.O(0,[667,139,744],function(){return n(n.s=7155)}),_N_E=n.O()}]);
