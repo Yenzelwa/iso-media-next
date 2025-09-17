82a5466983a7e6de5ef663786b00c69a7a700eb6
Nokukhanya Dumakude <NokukhanyaD@hollywoodbets.net>
2025-09-17 07:12:46 +0200

    updated packages

--- src/app/(root)/not-found.tsx
+++ src/app/(root)/not-found.tsx
@@ -10,7 +10,7 @@ const NotFoundPage = () => {
         <h1 className="text-6xl font-bold mb-4 text-red-600">404</h1>
         <h3 className="text-2xl font-semibold mb-2">Page not found!</h3>
         <p className="text-gray-400 mb-6">
-          The page you're looking for doesn't exist or has been moved.
+          The page you&apos;re looking for doesn&apos;t exist or has been moved.
         </p>
         <Link href="/">
           <span className="inline-block bg-red-700 hover:bg-red-600 text-white px-6 py-3 rounded-md transition duration-300">
